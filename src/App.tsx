2d75e99ca10eb68902cc996a4acab71e4e0461ac
Yaash Lodha <yaashlodha@gmail.com>
2026-06-25 11:35:46 +0530

    Added Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function App() {
   return (
